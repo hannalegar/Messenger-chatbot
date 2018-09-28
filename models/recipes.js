b2eb70a08626fed09ba7031e1026d290d7603ab7
hannalegar <hanna.legar@gmail.com>
2018-09-28 18:04:58 +0200

    mukodj ma

--- models/recipes.js
+++ models/recipes.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var RecipeSchema = new Schema({
     user_id: {type: String},
     title: {type: String},
-    ingredients: {type: Array<String>[]},
+    ingredients: {type: String},
     description: {type: String}
   });
   
